f62cd69feb43ba3a3e1a4459248dc52e1156e9f7
Sudoku Builder <sudoku.builder@gmail.com>
2013-07-02 21:56:15 +0200

    purge varnish cache, select all domains.

--- lib/sudoku_builder/builder.rb
+++ lib/sudoku_builder/builder.rb
@@ -4,7 +4,10 @@ module SudokuBuilder
     def perform_action(action)
       log_in(ENV['SUDOKU_USERNAME'], ENV['SUDOKU_PASSWORD'])
       page = @agent.get(@base_url + "/#{action}")
-      confirm_form = page.forms.second
+      confirm_form = page.form_with(action: /\/#{action}$/)
+      if action == 'purge'
+        confirm_form.checkboxes.each{|c| c.check}
+      end
       @current_page = @agent.submit(confirm_form)
     end
   end
