10bf33897c03698871b03c0698890778db5bcb9b
Sudoku Builder <sudoku.builder@gmail.com>
2013-07-02 16:28:00 +0200

    rake tasks, extract log in.

--- lib/sudoku_builder/builder.rb
+++ lib/sudoku_builder/builder.rb
@@ -2,6 +2,7 @@
 module SudokuBuilder
   class Builder < SudokuBuilder::Base
     def perform_action(action)
+      builder.log_in(ENV['SUDOKU_USERNAME'], ENV['SUDOKU_PASSWORD'])
       page = @agent.get(@base_url + "/#{action}")
       confirm_form = page.forms.second
       @current_page = @agent.submit(confirm_form)
--- lib/sudoku_builder/sudoku_tasks.rb
+++ lib/sudoku_builder/sudoku_tasks.rb
@@ -7,21 +7,22 @@ module SudokuBuilder
     rake_tasks do
       namespace :sudoku do
         task :login do
-          builder = SudokuBuilder::Builder.new(Rails.application.class.parent_name.downcase)
-          builder.log_in(ENV['SUDOKU_USERNAME'], ENV['SUDOKU_PASSWORD'])
         end
         desc 'Build package on onion/sudoku. Set onion credentials with SUDOKU_USERNAME and SUDOKU_PASSWORD env variables.'
-        task :build => :login do
+        task :build do
+          builder = SudokuBuilder::Builder.new(Rails.application.class.parent_name.downcase)
           builder.perform_action('force-build')
         end
 
         desc 'Restart onion/sudoku application. Set onion credentials with SUDOKU_USERNAME and SUDOKU_PASSWORD env variables.'
-        task :restart => :login do
+        task :restart do
+          builder = SudokuBuilder::Builder.new(Rails.application.class.parent_name.downcase)
           builder.perform_action('restart')
         end
 
         desc 'Purge onion/sudoku varnish caches. Set onion credentials with SUDOKU_USERNAME and SUDOKU_PASSWORD env variables.' 
-        task :purge_varnish => :login do
+        task :purge_varnish do
+          builder = SudokuBuilder::Builder.new(Rails.application.class.parent_name.downcase)
           builder.perform_action('purge')
         end
       end
